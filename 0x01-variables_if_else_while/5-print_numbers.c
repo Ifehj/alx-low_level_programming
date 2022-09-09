56766d0f22083bf36bf5c6455546486fa0d0874f
ADESANYA IFEOLUWA <adesanya7ife9@gmail.com>
2022-09-09 16:02:15 -0700

    printing all numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers
+ * followed by a new line
+ * Return: 0 if true
+ */
+int main (void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
