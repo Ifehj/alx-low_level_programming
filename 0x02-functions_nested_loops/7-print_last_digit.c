ef33bf41593975090a23636235a089b0303af070
ADESANYA IFEOLUWA <adesanya7ife9@gmail.com>
2022-09-13 18:50:18 -0700

    correction

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_last_digit - prints the last digit of a number
- *
+ * @n: the number in quesion
  * Return: Value of the last digit
  */
 int print_last_digit(int n)
