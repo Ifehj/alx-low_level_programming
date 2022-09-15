f6e8e5cd907052ca5f20a378aa16804ef6d57b9c
ADESANYA IFEOLUWA <adesanya7ife9@gmail.com>
2022-09-15 05:23:02 -0700

    correction

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _isupper - checkes the case of a letter
+ * _isupper - checks the case of a letter
  * @c: character to be tested
  * Return: 1 if true and 0 otherwise
  */
 
 int _isupper(int c)
 {
-	if ((c >='A') && (c <= 'Z'))
+	if ((c >= 'A') && (c <= 'Z'))
 	{
 		return (1);
 	}
