106b5701623b1208007c2ca87021de45ac9a8330
ADESANYA IFEOLUWA <adesanya7ife9@gmail.com>
2022-09-15 06:01:05 -0700

    correction

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -17,7 +17,7 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-int putchar(char);
+int _putchar(char);
 void print_number(int n);
 
 #endif /* MAIN_H */
