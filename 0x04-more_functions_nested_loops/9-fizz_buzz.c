67394bfb0786aa689fe628dbeb78681bdd923eb9
ADESANYA IFEOLUWA <adesanya7ife9@gmail.com>
2022-09-15 11:46:42 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints either number, fuzz, buzz or fizzbuzz
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num = 1;
+
+	while (num++ < 100)
+	{
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("fizzBuzz ");
+		}
+		else if ((num % 3) == 0)
+		{
+			printf("Fuzz ");
+		}
+		else if ((num % 5) == 0)
+		{
+			if (num != 100)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz");
+			}
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
