ea77f17e926488d47b51ef60a3ae71ad80d04add
ADESANYA IFEOLUWA <adesanya7ife9@gmail.com>
2022-10-12 19:52:25 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,6 +3,7 @@
 /**
  * int_index - searches for an integer
  * @array: array to search in
+ * @size: size of the array
  * @cmp: pointer to function
  * Return: index for the first element for which the cmp
  * function does not return 0, otherwise -1
