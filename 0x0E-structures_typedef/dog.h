623e017af72246d9b2e94c2e536d4ce022ebc171
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2023-06-21 08:57:47 +0100

    betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-#include <stddef.h>
-
 
 /**
  * struct dog - contains dog information
@@ -25,6 +23,7 @@ struct dog
  */
 
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
