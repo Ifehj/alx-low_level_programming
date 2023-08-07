3705d7f972ff3e9d45e39d3e80caabb26136e52e
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2023-08-07 23:04:27 +0100

    type error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,13 +5,13 @@
  * @filename: filename to be read from.
  * @letters: letters is the number of letters it should read and print.
  *
- * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
+ * Return: If the function fails or filename is NULL - 0
+ * O/w - the actual number of bytes the function can read and print.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int *fd, i;
+	int fd;
 	ssize_t num_byte, num_read;
 	char *buf;
 
