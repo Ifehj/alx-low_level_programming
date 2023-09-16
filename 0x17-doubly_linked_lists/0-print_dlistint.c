b071ad8f5894a9ec7c9882e6aac8fa0f14bb9330
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2023-09-16 20:27:05 +0100

    betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,6 +11,7 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *current;
 	int node_num = 0;
+
 	current = h;
 
 	while (current)
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -10,8 +10,8 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	const dlistint_t *current;
-	
 	int node_num = 0;
+	
 	current = h;
 
 	while (current)
