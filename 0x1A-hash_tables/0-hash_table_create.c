d2b0ce869285d386f5cfdaf418e838e5da8f33b4
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2023-10-07 00:51:26 +0100

    hash tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a new hash table
+ * @size: Size of the hash table
+ * 
+ * Return: Pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *Table;
+	unsigned long int i;
+
+	Table = malloc(sizeof(hash_table_t));
+	if (Table == NULL)
+		return NULL;
+	Table->size = size;
+	Table->array = malloc(szie * sizeof(hash_node_t *));
+	if (table->array == NULL)
+	{
+		free(Table);
+		return NULL;
+	}
+	for(i = 0; i < size; i++)
+	{
+		Table->array[i] = NULL;
+	}
+
+	return (Table);
+}
