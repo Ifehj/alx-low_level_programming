8cdd4e96f516d7235f0aa6f121c639632b6a4140
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2023-10-07 15:51:44 +0100

    betty validation

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -3,13 +3,13 @@
  * key_index - gives the index of a key
  * @key: key character
  * @size: size of the array of the hash table
- * 
+ *
  * Returns:  index at which the key/value pair should be stored in the array of the hash table
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash;
-	
-	hash =  hash_djb2(key);
+
+	hash = hash_djb2(key);
 	return (hash % size);
 }
