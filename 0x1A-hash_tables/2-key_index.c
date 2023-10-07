f3cbf312de39c1d84e791d00bb76cbeebe72a7f6
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2023-10-07 15:54:37 +0100

    betty validation

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -4,7 +4,8 @@
  * @key: key character
  * @size: size of the array of the hash table
  *
- * Returns:  index at which the key/value pair should be stored in the array of the hash table
+ * Return:  index at which the key/value pair should be stored
+ * in the array of the hash table
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
