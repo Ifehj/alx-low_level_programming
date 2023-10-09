69a46c6a77a58116fa7417f735a34e84bf6d3942
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2023-10-09 23:21:56 +0100

    remove files

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/ython3
-""" Returns all attribute and omethods of the object
-@obj: object to check
- """
-
-
-def lookup(obj):
-    return (dir(obj))
--- 0x0A-python-inheritance/5-base_geometry.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-""" Empty class """
-
-class BaseGeometry:
-	pass
